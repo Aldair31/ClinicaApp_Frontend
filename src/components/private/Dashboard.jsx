58309252bf449d957e972d3e217deb5981810a01
Daniel <=>
2021-10-15 01:22:47 -0500

    subir imagenes con cambio reactivo

--- src/components/private/Dashboard.jsx
+++ src/components/private/Dashboard.jsx
@@ -164,7 +164,7 @@ const Dashboard = ({ usuario, logout }) => {
 								</>
 							) : null}
 							<div className="itemLong">
-								<button
+								<NavLink to='/'
 									className="item"
 									onClick={() => {
 										logout();
@@ -178,7 +178,7 @@ const Dashboard = ({ usuario, logout }) => {
 									>
 										<path d="M0 2v20h14v-2h-12v-16h12v-2h-14zm18 7.408l2.963 2.592-2.963 2.592v-1.592h-8v-2h8v-1.592zm-2-4.408v4h-8v6h8v4l8-7-8-7z" />
 									</svg>
-								</button>
+								</NavLink>
 								Salir
 							</div>
 						</div>
--- src/components/private/extras/FormApoderado.jsx
+++ src/components/private/extras/FormApoderado.jsx
@@ -6,7 +6,6 @@ import { connect } from 'react-redux';
 const FormApoderado = (usuario) => {
 	const [state, setEstado] = useState({ file: null });
 	const subirArchivo = (e) => {
-		// console.log(e.target.files, "$$$$");console.log(e.target.files, "$$$$");
 		let file = e.target.files[0];
 		setEstado({ file: file });
 		console.log(file);
@@ -16,9 +15,7 @@ const FormApoderado = (usuario) => {
 		let file = state.file;
 		let formdata = new FormData();
 		formdata.append('avatar', file);
-		// formdata.append('name', 'avatar')
-		console.log('Subido');
-    console.log('usuario id:'+usuario.uid);
+
 		axios({
 			url: `${url}/api/auth/files`,
 			method: 'POST',
@@ -27,18 +24,69 @@ const FormApoderado = (usuario) => {
 				id: usuario.usuario.uid,
 			},
 		}).then((res) => {
+			console.log('....');
 			console.log(res);
+			console.log('.....');
+			let [user] = usuario.datos.filter((item) => item._id === usuario.usuario.uid)
+			user.foto_perfil = res.data.usuario.foto_perfil
+			console.log(user);
+			usuario.set_datos(usuario.datos.filter((item) => item._id !== usuario.usuario.uid ))
+
+			// user.foto_perfil= res.file.name
+		    usuario.set_datos([...usuario.datos,{user}])
 		});
 	};
 	return (
-		<div className="contenedor_perfil">
-			{console.log('----------')}
-			{console.log(usuario)}
-			{console.log('----------')}
-			<form method="post" encType="multipart/form-data">
+		<div
+			className="contenedor_perfil"
+			style={{
+				background: '#0000006e',
+				width: '100%',
+				height: '100vh',
+				position: 'absolute',
+				top: '0px',
+				left: '0px',
+				zIndex: '33',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center',
+				// marginTop:'-80px'
+			}}
+		>
+			<div
+				className="cerrarForm"
+				style={{
+					background: '#ffffff',
+					padding: '12px',
+					position: 'absolute',
+					top:'0px',
+					right: '0px',
+					cursor: 'pointer'
+				}}
+				onClick={() => {
+					usuario.setForm(false);
+				}}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+				>
+					<path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z" />
+				</svg>
+			</div>
+			<form
+				method="post"
+				encType="multipart/form-data"
+				style={{
+					background: '#ffffff',
+					padding: '20px',
+					borderRadius: '15px',
+				}}
+			>
 				<div>
-					{/* <label>Seleccione archivo</label> */}
-					<i className="far fa-user"></i>
 					<br />
 					<input
 						type="file"
