1a81d9ecb2a25e7899cc1e29ced3876314a8f4e1
Aldair Díaz Ramón <diazramonaldair.1297@gmail.com>
2021-10-15 19:10:08 -0500

    Consumiendo datos de Historia Clinica, Consultas y Antecedentes

--- src/components/private/extras/HistoriasClinicas.jsx
+++ src/components/private/extras/HistoriasClinicas.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 import url from '../../../keys/backend_keys';
+import Antecedentes from './Antecedentes';
 const HistoriasClinicas = ({ id }) => {
 	const [datos, setDatos] = useState([]);
 	useEffect(() => {
@@ -9,11 +11,70 @@ const HistoriasClinicas = ({ id }) => {
 				setDatos(data);
 			});
 	}, []);
+	// const [citas, setCitas] = useCita([]);
+	
+	const [citas, setCitas] = useState([]);
+	useEffect(() => {
+		fetch(`${url}/Historia`)
+			.then((resp) => {
+				return resp.json();
+			})
+			.then((datos) => {
+				console.log('respuesta');
+				console.log(datos);
+				setCitas(datos);
+			});
+	}, []);
 	return (
 		<div>
-             {console.log(datos)}
-			Historias clinicas <div>
-				
+			<div className="list">
+				<h2>HISTORIA CLINICA</h2>
+				<div>
+				<div className="citas">
+					{citas.map((item) => {
+						return (
+							<>
+								{item._id === id? (
+									<div key={item._id} className="cita">
+										<h3>Datos de Filiación</h3>
+										<p>Dirección: {item.direccion}</p>
+										<p>DNI: {item.dni_paciente}</p>
+										<p>Nombre: {item.nombres_paciente}</p>
+										<p>Referencia: {item.referencia}</p>
+									</div>
+								) : null}
+							</>
+						);
+						
+					})}
+				</div>
+				</div>
+				<div className="citas">
+					{datos.map((item) => {
+						console.log(item)
+						return (
+							<>
+								{item.id_Historia===id? (
+									<div key={item._id} className="cita">
+										<h3>Historia</h3>
+										<p>Fecha: {moment(item.fecha_nac).format('DD-MM-YYYY')}</p>
+										<p>Diagnostico: {item.diagnostico}</p>
+										<p>Tratamiento: {item.tratamiento}</p>
+										<p>Examenes Auxiliares: {item.examenesAuxiliares}</p>
+										{/* <NavLink to={`/historia/${item._id}`}>
+											<p className="ver">
+													Antecedentes
+											</p>
+										</NavLink> */}
+									</div>
+								) : null}
+							</>
+						);
+					})}
+				</div>
+				<div>
+					<Antecedentes id={id}/>
+				</div>
 			</div>
 		</div>
 	);
