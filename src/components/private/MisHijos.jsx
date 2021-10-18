e7b27197adee2eb1a6504b1759c8ac0216591006
Daniel <=>
2021-10-17 19:51:06 -0500

    Consumiendo hijos en componente de padres

--- src/components/private/MisHijos.jsx
+++ src/components/private/MisHijos.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import url from '../../keys/backend_keys';
+const MisHijos = ({ usuario }) => {
+	const [hijos, setHijos] = useState([]);
+	const uid = usuario.uid;
+	useEffect(() => {
+		fetch(`${url}/api/auth/${uid}`)
+			.then((resp) => resp.json())
+			.then((datos) => {
+				setHijos(datos.hijos);
+			});
+	}, [uid]);
+	return (
+		<div>
+			{hijos.map((item) => {
+				return <div>{item.nombres_paciente}</div>;
+			})}
+		</div>
+	);
+};
+const mapStateToProps = (state) => ({
+	usuario: state.usuario,
+});
 
-const MisHijos = () => {
-    return (
-        <div>
-            klklklklklklklklklklk
-        </div>
-    )
-}
-
-export default MisHijos
+export default connect(mapStateToProps)(MisHijos);
+// export default MisHijos
