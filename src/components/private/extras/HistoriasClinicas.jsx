1f27af7c98d0bf0243d47fa4bcf5635e44a3ace7
DavidVidaurre <luisdavidmaquenvidaurre@gmail.com>
2021-11-14 15:24:57 -0500

    Se agregó edad esperada en la historia clínica

--- src/components/private/extras/HistoriasClinicas.jsx
+++ src/components/private/extras/HistoriasClinicas.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 import url from '../../../keys/backend_keys';
 import { Link } from 'react-router-dom';
 
+import useHistClinica from '../../../hooks/useHistClinica'
+
 
 const HistoriasClinicas = () => {
 	const { id } = useParams();
@@ -13,6 +15,17 @@ const HistoriasClinicas = () => {
 			.then((resp) => resp.json())
 			.then((data) => setDatos(data));
 	}, []);
+
+	let {fechaHistoria, fechaNac, pesoPaciente} = useHistClinica()
+
+	//console.log(moment(fechaNac[0]))
+	//console.log(moment(fechaNac[0]).format())
+
+	let fechaNacimiento = []
+
+	for (let item in fechaNac){
+		fechaNacimiento[item] = moment(fechaNac[0]).format()
+	}
 
 	return (
 		<div>
@@ -26,6 +39,9 @@ const HistoriasClinicas = () => {
 										<h3>Historia Clinica</h3>
 										<p>
 											Fecha : {moment(item.fecha).format('DD/MM/YYYY')}
+										</p>
+										<p>
+											Edad esperada : {moment(item.fecha).diff(fechaNacimiento[0], 'months')} meses
 										</p>
 										<p>
 											Diagnostico: {item.diagnostico}
