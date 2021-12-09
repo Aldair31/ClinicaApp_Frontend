16d9b1c1fed0735065780edc9b982560574f43fc
Aldair Díaz Ramón <diazramonaldair.1297@gmail.com>
2021-12-08 22:45:12 -0500

    Se reemplazo calendario para reserva de citas

--- src/components/private/InicioSecretaria.jsx
+++ src/components/private/InicioSecretaria.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import Calendar from 'react-calendar'
-import { useState } from 'react';
+import React, { useState }from 'react';
 import '../../sass/Calendario.sass';
-import '../../sass/Dashboard.sass';
-import useCita from '../../hooks/useCita';
 import url from '../../keys/backend_keys';
 import moment from 'moment';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid';// a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
 // import 'react-calendar/dist/Calendar.css'
 // import MostrarCita from './MostrarCita';
 
 const InicioSecretaria =  () => {
     const [state, setState] = useState(false);
-
     const [value, onChange] = useState(new Date());
 
     // const mostrar = () =>{
@@ -20,17 +18,6 @@ const InicioSecretaria =  () => {
     //     })
     // }
     console.log(value)
-    let [datos_af, loading, set_datos_af] = useCita();
-	const switchMotivo = (valor) => {
-		switch (valor) {
-			case 1:
-				return 'Vacuna';
-			case 2:
-				return 'Consulta pediátrica';
-			default:
-				return 'Control de crecimiento';
-		}
-	};
 	const onDelete = (id) => {
 		fetch(`${url}/Cita/${id}`, {
 			headers: {
@@ -48,152 +35,78 @@ const InicioSecretaria =  () => {
 				set_datos_af(datos_af.filter((item) => item._id !== id));
 			});
 	};
-
- return (
-     <>
-        <div >
-            <div className='list'>
-                <h2 style={{textAlign:'center'}}>CALENDARIO DE CITAS</h2>
-            </div>
-            <div className='calendar'>
-                <Calendar
-                onChange={onChange}
-                value={value}
-                // tileClassName="content"
-                tileClassName={({ fecha, view }) => {
-                    if(datos_af.find(x=>x===moment(fecha).format("DD-MM-YYYY"))){
-                     return  'content'
-                    }
+    const handleDateClick = (arg) => { // bind with an arrow function
+        // alert(arg.dateStr);
+        if(arg.dateStr){
+            setState(true)
+        }
+    }
+    const Modal = () => {
+        return (
+            
+            <div
+                style={{
+                    background: '#00000039',
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    height: '100vh',
+                    width: '100%',
+                    zIndex:'2',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                 }}
-              
-                />
-            </div>
-             <div >
-                {state ? (
-                <>
-                {/* <div>
-                    <MostrarCita/>
-                    {value.toString()}
-                    {value}
-                   { mostrar()}
-                </div> */}
-                <div style={{
-						background: '#00000039',
-						position: 'absolute',
-						top: '0',
-						left: '0',
-						height: '100vh',
-						width: '100%',
-	
-						display: 'flex',
-						justifyContent: 'center',
-						alignItems: 'center',
-                }}>
-                    <div style={{
+            >
+                <form
+                    style={{
                         background: '#ffffff',
-                        padding: '8px',
-                        width:'40%',
+                        padding: '2px',
                         borderRadius: '6px',
-                    }}>
-                    <h3 style={{textAlign:'center', fontSize:'20px', marginTop:'10px'}}>CITAS DEL {value.toLocaleDateString()}</h3>
-                    {/* {value.toLocaleDateString()} */}
-                    {loading !== null ? (
-                        <div className="dato_filiacion" style={{marginLeft:'25px', marginBottom:'20px',overflowY: 'scroll', height:'72vh'}}>
-                            {datos_af.map((item) => (
-                                <div className="dato_filiacion"  key={item._id}>
-                                    {console.log(moment(item.fecha).format('DD/MM/YYYY'))}
-                                    {moment(item.fecha).format('DD/MM/YYYY') === value.toLocaleDateString() ? (
-                                        <div style={{marginLeft:'10%', marginBottom:'20px', backgroundColor:'#f4f4f4', padding:'20px', marginRight:'10%'}}>
-                                            <p>
-                                                <strong>-Hora de la cita: </strong>
-                                                {
-                                                    moment(item.fecha).format('LT')
-                                                }
-                                            </p>
-                                            <p>
-                                                <strong>-DNI del paciente: </strong>
-                                                {item.DNI}
-                                            </p>
-                                            <p>
-                                                <strong>
-                                                    -Nombres del paciente:{' '}
-                                                </strong>
-                                                {item.nombre_paciente}
-                                            </p>
-                                            <p>
-                                                <strong>-Fecha de nacimiento: </strong>
-                                                {
-                                                    moment(item.fecha_nac).format('DD/MM/YYYY')
-                                                }
-                                            </p>
-                                            {/* <p>
-                                                <strong>-Fecha de la cita: </strong>
-                                                {
-                                                    moment(item.fecha).format('DD/MM/YYYY')
-                                                }
-                                            </p> */}
-                                            
-                                            
-                                            
-                                            <p>
-                                                <strong>-Responsable: </strong>
-                                                {item.responsable}
-                                            </p>
-                                            <p>
-                                                <strong>-Telefono: </strong>
-                                                {item.telefono}
-                                            </p>
-                                            <p>
-                                                <strong>-Motivo: </strong>
-                                                <span>
-                                                    {switchMotivo(item.motivo)}
-                                                </span>
-                                            </p>
-                                            <p>
-                                                <strong>-Sexo: </strong>
-                                                <span>
-                                                    {item.sexo === 1
-                                                        ? 'Hombre'
-                                                        : 'Mujer'}
-                                                </span>
-                                            </p>
-                                            <p>
-                                                <strong>-Condición: </strong>
-                                                <span>
-                                                    {item.condicion === 1
-                                                        ? 'Nuevo'
-                                                        : 'Continuador'}
-                                                </span>
-                                            </p>
-                                            <p>
-                                            &nbsp;&nbsp;
-                                            <strong
-                                                onClick={() => {
-                                                    onDelete(item._id);
-                                                }}
-                                                style={{
-                                                    cursor: 'pointer',
-                                                    color: 'crimson',
-                                                }}
-                                            >
-                                                Eliminar
-                                            </strong>
-                                            </p>
-                                        </div>
-                                    ) : null}
-                                    
-                                </div>
-                            ))}
+                    }}
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        
+                    }}
+                >
+                    <div className='calendario'>
+                        <h3>
+                            REGISTRAR RESERVA PARA {moment(value.dateStr).format('DD/MM/YYYY')}
+                        </h3>
+                        <div className='formularioModal'>
+                        <p>
+                            <label>Paciente:  </label>
+                            <input
+                                type='text'
+                                name='paciente'
+                                placeholder="Paciente"
+                               
+                            />
+                        </p>
+                        <p>
+                            <label>Hora:  </label>
+                            <input
+                                type='time'
+                                name='hora'
+                                placeholder="Paciente"
+                                
+                                
+                                className='hora'
+                                
+                            />
+                        </p>
+                        <p>
+                            <button>Aceptar</button>
+                        </p> 
                         </div>
-				    ) : null}
+                        
                     </div>
                     
-                </div>
-                <div>
-                    <button
+                    <br />
+                </form>
+                <button
                     onClick={() => {
                         setState(false);
-                       
                     }}
                     style={{
                         position: 'absolute',
@@ -203,41 +116,67 @@ const InicioSecretaria =  () => {
                         padding: '18px',
                         cursor: 'pointer',
                     }}
-                    >
-                         
-                        <i
-                            class="fas fa-times"
-                            style={{ fontSize: '19px' }}
-                        ></i>
-                    </button>
-                </div>
-                </>): null}   
-            </div>
-            <br />
-            <div>
-                <button
-                    onClick={() => {
-                        setState(true);
-                    }}
-                    style={{
-                        padding: '9px',
-                        color: 'white',
-                        border: 'none',
-                        background: '#3ec4ab',
-                        marginBottom: '11px',
-                        borderRadius: '11px',
-                        cursor: 'pointer',
-                        marginLeft:'400px'
-                    }}
                 >
-                    Mostrar Cita
-                    
+                    <i
+                        class="fas fa-times"
+                        style={{ fontSize: '19px' }}
+                    ></i>
                 </button>
             </div>
-        </div>
-     </>
- )
-    
+        );
+    }
+
+    return (
+        <>
+        {state && <Modal/>}
+                
+            <div style={{ width:'90%', marginTop:'15px'}}>
+            <FullCalendar
+                plugins={[ dayGridPlugin, interactionPlugin]}
+                locale='es'
+                height='85vh'
+                initialView="dayGridMonth"
+                // ref={calendarRef}
+                headerToolbar={{
+                    right:'today prev,next',
+                    center:'title',
+                    left:'dayGridMonth',
+                }}
+                buttonText={{
+                    today: 'Hoy',
+                    month: 'Mes',
+                }}
+                // 
+                events={[
+                    { title: '13:58 - diego', date: '2021-12-01T10:30:00'},
+                    { title: 'event 2', date: '2021-12-01T10:32:00' },
+                    { title: '13:58', date: '2021-12-01T10:33:00' },
+                    { title: 'event 2', date: '2021-12-04' },
+                    { title: '13:58', date: '2021-12-03' },
+                    { title: 'event 2', date: '2021-12-04' },
+                    { title: 'event 2', date: '2021-11-10' }
+                    ]}
+                editable= {true}
+                selectable={true}
+                select={handleDateClick}
+                dateClick={[handleDateClick,onChange]}
+                dayMaxEventRows={ true} // for all non-TimeGrid views
+                view={{
+                    timeGrid: {
+                    dayMaxEventRows: 10 // adjust to 6 only for timeGridWeek/timeGridDay
+                    }
+                }}
+                eventClick={(info)=>{
+                    console.log('click', info)
+                }}
+                eventRemove={true}
+                onChange={onChange}
+                value={value}
+                // showNonCurrentDates={false}
+            />
+            </div>
+        </>
+    )   
 }
 
 export default InicioSecretaria;
